b93458d7eaef4482235172349ffc08bde9a15db5
Ricardo Monterrosa <rmonterrosa@MacBook-Pro-de-Ricardo.local>
2021-10-01 10:06:49 -0500

    modified createReducer

--- src/helpers/createReducer/index.js
+++ src/helpers/createReducer/index.js
@@ -1,13 +1,9 @@
-const useCreateReducer = () => {
-  const createReducer = (initialState, handler) => {
-    return (state = initialState, action) => {
-      return handler?.hasOwnProperty(action.type) ? handler[action.type](state, action) : state;
-    };
-  };
-
-  return {
-    createReducer,
-  };
-};
+const useCreateReducer =
+  () =>
+  (initialState, handler) =>
+  (state = initialState, action) =>
+    Object.prototype.hasOwnProperty.call(handler, action.type)
+      ? handler[action.type](state, action)
+      : state;
 
 export default useCreateReducer;
